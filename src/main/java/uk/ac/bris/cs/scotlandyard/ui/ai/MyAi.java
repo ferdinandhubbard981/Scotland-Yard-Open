9ea29a53358acc75e21a514c361ce2e77a01e972
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2022-03-23 11:43:54 +0000

    skeleton files

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -0,0 +1,26 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.annotation.Nonnull;
+
+
+import io.atlassian.fugue.Pair;
+import uk.ac.bris.cs.scotlandyard.model.Ai;
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+
+public class MyAi implements Ai {
+
+	@Nonnull @Override public String name() { return "Name me!"; }
+
+	@Nonnull @Override public Move pickMove(
+			@Nonnull Board board,
+			Pair<Long, TimeUnit> timeoutPair) {
+		// returns a random move, replace with your own implementation
+		var moves = board.getAvailableMoves().asList();
+		return moves.get(new Random().nextInt(moves.size()));
+	}
+}
