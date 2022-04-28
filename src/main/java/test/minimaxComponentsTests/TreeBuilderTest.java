501197a0b28a5b458f16617fe7968b9898764c51
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-04-28 01:58:34 +0100

    added 3 tests

--- src/main/java/test/minimaxComponentsTests/TreeBuilderTest.java
+++ src/main/java/test/minimaxComponentsTests/TreeBuilderTest.java
@@ -1,7 +1,6 @@
 package test.minimaxComponentsTests;
 
 import org.junit.jupiter.api.Test;
-import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.ui.ai.minimaxComponents.MinimaxGame;
 import uk.ac.bris.cs.scotlandyard.ui.ai.minimaxComponents.MinimaxGameState;
 import uk.ac.bris.cs.scotlandyard.ui.ai.minimaxComponents.Node;
@@ -9,13 +8,21 @@ import uk.ac.bris.cs.scotlandyard.ui.ai.minimaxComponents.Node;
 import java.util.List;
 
 public class TreeBuilderTest {
-    static class DummyMinimaxGameState implements MinimaxGameState {
-        private final List<MinimaxGameState> childStates;
+    static class DummyMove {
+        private final int moveNumber;
+        public DummyMove(int dummyMoveNumber) {this.moveNumber = dummyMoveNumber;}
+
+        public int getMoveNumber() {
+            return moveNumber;
+        }
+    }
+    static class DummyMinimaxGameState implements MinimaxGameState<DummyMove> {
+        private final List<MinimaxGameState<DummyMove>> childStates;
         private final boolean isGameFinished;
         private final int score;
-        private final Move moveLeadingToState;
+        private final DummyMove moveLeadingToState;
         @Override
-        public List<MinimaxGameState> getChildStates() {
+        public List<MinimaxGameState<DummyMove>> getChildStates() {
             return this.childStates;
         }
 
@@ -30,12 +37,12 @@ public class TreeBuilderTest {
         }
 
         @Override
-        public Move getMoveLeadingToState() {
+        public DummyMove getMoveLeadingToState() {
             return this.moveLeadingToState;
         }
 
-        public DummyMinimaxGameState(boolean isGameFinished, int score, Move moveLeadingToState, MinimaxGameState ...children){
-            this.childStates = List.of(children);
+        public DummyMinimaxGameState(boolean isGameFinished, int score, DummyMove moveLeadingToState, List<MinimaxGameState<DummyMove>> children){
+            this.childStates = children;
             this.isGameFinished = isGameFinished;
             this.moveLeadingToState = moveLeadingToState;
             this.score = score;
@@ -44,20 +51,77 @@ public class TreeBuilderTest {
 
     @Test
     public void testDummyGameStateCanBuild(){
-        MinimaxGame game = new MinimaxGame();
+        MinimaxGame<DummyMove> game = new MinimaxGame<>();
         game.buildTree(new DummyMinimaxGameState(true, 10, null, null), true);
         assert game.getBestMove() == null;
-        Node best = game.getBestNode();
+        Node<DummyMove> best = game.getBestNode();
         assert best.isMaximisingPlayer();
         assert best.getChildNodes() == null;
         assert best.getParentNode() == null;
-
     }
 
     @Test
-    public void testGameStateChildrenCanBuild(){
-        MinimaxGame game = new MinimaxGame();
+    public void testGameStateWithChildrenCanBuild(){
+        MinimaxGame<DummyMove> game = new MinimaxGame<>();
+        DummyMinimaxGameState parent = new DummyMinimaxGameState(
+                false,
+                15,
+                null,
+                List.of(new DummyMinimaxGameState(
+                        false, 20, new DummyMove(93132), null)
+                ));
+        game.buildTree(parent, true);
+        Node<DummyMove> best = game.getBestNode();
+        assert best.getParentNode().getScore() == 15;
+        assert best.getParentNode().isMaximisingPlayer();
+        assert best.getParentNode().getParentNode() == null;
+        assert !best.isMaximisingPlayer();
+        assert best.getChildNodes() == null;
+        assert best.getScore() == 20;
+        assert game.getBestMove().getMoveNumber() == 93132;
+    }
 
+    @Test
+    public void testGameStateReturnsBestNodeTwoLayer(){
+        MinimaxGame<DummyMove> game = new MinimaxGame<>();
+        DummyMinimaxGameState root = new DummyMinimaxGameState(
+                false,
+                30,
+                null,
+                List.of(
+                        new DummyMinimaxGameState(false, -5, new DummyMove(1), List.of()),
+                        new DummyMinimaxGameState(false, 10, new DummyMove(2), List.of()),
+                        new DummyMinimaxGameState(true, 5, new DummyMove(3), List.of())
+                )
+        );
+        game.buildTree(root, true);
+        Node<DummyMove> bestNode = game.getBestNode();
+        assert !bestNode.isMaximisingPlayer();
+        assert bestNode.getScore() == 10;
+        assert !bestNode.getGameState().isGameFinished();
+        assert game.getBestMove().getMoveNumber() == 2;
+    }
+
+    @Test
+    public void testGameStateOnUnevenTree(){
+        MinimaxGame<DummyMove> game = new MinimaxGame<>();
+        DummyMinimaxGameState root = new DummyMinimaxGameState(
+                false,
+                -12313,
+                null,
+                List.of(
+                        new DummyMinimaxGameState(true, -10000000, new DummyMove(1), null),
+                        new DummyMinimaxGameState(false, 10, new DummyMove(2), null),
+                        new DummyMinimaxGameState(false, -125, new DummyMove(3),
+                                List.of(new DummyMinimaxGameState(false, 70000, new DummyMove(4), null)))
+                ));
+        game.buildTree(root, false);
+        Node<DummyMove> best = game.getBestNode();
+        assert best.isMaximisingPlayer();
+        assert best.getScore() == 70000;
+        assert !best.getGameState().isGameFinished();
+        assert best.getGameState().getMoveLeadingToState().getMoveNumber() == 4;
+        assert game.getBestMove().getMoveNumber() == 3;
     }
 
 
