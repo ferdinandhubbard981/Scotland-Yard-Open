669019c22483e5eddc8f463a114ea0f2bd3171fb
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-04-29 01:10:56 +0100

    added TutorialTest for Ai. use similar framework

---
+++ src/main/java/test/aiTests/AiTestRunner.java
@@ -0,0 +1,11 @@
+package test.aiTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TutorialTest.class
+})
+
+public class AiTestRunner {}
