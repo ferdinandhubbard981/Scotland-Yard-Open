fc3d6aa568b5460283687df9f72dd73653f1eac4
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-05-02 22:22:24 +0100

    made MCTS.hasTimeLeft public to test. added skeleton tests

--- src/main/java/test/aiTests/AiTestRunner.java
+++ src/main/java/test/aiTests/AiTestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        NNetInputTest.class
+        MCTSTest.class,
+        NeuralNetTest.class
 })
 
 public class AiTestRunner {}
--- src/main/java/test/aiTests/MCTSTest.java
+++ src/main/java/test/aiTests/MCTSTest.java
@@ -1,5 +1,6 @@
 package test.aiTests;
 
+import org.junit.Test;
 import uk.ac.bris.cs.scotlandyard.ui.ai.MCTS;
 
 import java.util.List;
@@ -8,23 +9,49 @@ import java.util.Objects;
 public class MCTSTest {
     public testMethods<Float> testerMethods = new testMethods<>();
 
-    public void testGetActionProb1() {
+    @Test public void testGetActionProb1() {
         MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0, 0), List.of());
     }
 
-    public void testGetActionProb2() {
+    @Test public void testGetActionProb2() {
         MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0, 0), List.of());
     }
 
-    public void testGetActionProb3() {
+    @Test public void testGetActionProb3() {
         MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0, 0), List.of());
     }
 
-    public void testGetActionProb4() {
+    @Test public void testGetActionProb4() {
         MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0, 0), List.of());
     }
+
+    @Test public void testGetActionProb5() {
+        MCTS testObj = new MCTS(null, null);
+        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0, 0), List.of());
+    }
+
+    @Test public void testHasTimeLeft1() {
+        MCTS testObj = new MCTS(null, null);
+        assert testObj.hasTimeLeft(1) == false;
+    }
+
+    @Test public void testHasTimeLeft2() {
+        MCTS testObj = new MCTS(null, null);
+        assert testObj.hasTimeLeft(0) == true;
+    }
+
+    @Test public void testHasTimeLeft3() {
+        MCTS testObj = new MCTS(null, null);
+        assert testObj.hasTimeLeft(500) == false;
+    }
+
+    @Test public void testHasTimeLeft4() {
+        MCTS testObj = new MCTS(null, null);
+        assert testObj.hasTimeLeft(213132143) == false;
+    }
+
 }
