b93a034cb4baace51b465692eb5e43de95f506a4
ferdi <ferdinandhubbard@gmail.com>
2022-05-03 01:08:17 +0100

    flowcharts

--- src/main/java/test/aiTests/MCTSTest.java
+++ src/main/java/test/aiTests/MCTSTest.java
@@ -6,25 +6,25 @@ import java.util.List;
 import java.util.Objects;
 
 public class MCTSTest {
-    public testMethods<Float> testerMethods = new testMethods<>();
-
-    public void testGetActionProb1() {
-        MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
-    }
-
-    public void testGetActionProb2() {
-        MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
-    }
-
-    public void testGetActionProb3() {
-        MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
-    }
-
-    public void testGetActionProb4() {
-        MCTS testObj = new MCTS(null, null);
-        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
-    }
+//    public testMethods<Float> testerMethods = new testMethods<>();
+//
+//    public void testGetActionProb1() {
+//        MCTS testObj = new MCTS(null, null);
+//        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+//    }
+//
+//    public void testGetActionProb2() {
+//        MCTS testObj = new MCTS(null, null);
+//        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+//    }
+//
+//    public void testGetActionProb3() {
+//        MCTS testObj = new MCTS(null, null);
+//        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+//    }
+//
+//    public void testGetActionProb4() {
+//        MCTS testObj = new MCTS(null, null);
+//        assert testerMethods.hasSameValues(testObj.getActionProb(null, 0), List.of());
+//    }
 }
